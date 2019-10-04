fc78c71d542629801c36c50bf946e6cee5ac8429
owenmadin <owen.madin@colorado.edu>
2019-10-04 00:01:17 -0600

    Added profile results

--- plot_gtex.py
+++ plot_gtex.py
@@ -23,8 +23,31 @@ def linear_search(key, L):
 
 
 
-def binary_serach(key, L):
-    pass
+def binary_search(key, L):
+    
+    if key is None:
+        raise TypeError('linear_search: must supply a key')
+    if isinstance(key, list):
+        raise TypeError('linear_search: key must be a single value')
+    if L is None:
+        raise TypeError('linear_search: must supply a list')
+    if not isinstance(L,list):
+        raise TypeError('linear_search: list must be of type "list"')
+        
+    for i in range(len(L)):
+        lo = -1
+        hi = len(L)
+    while (hi - lo > 1):
+        mid = (hi + lo) // 2
+
+        if key == L[mid][0]:
+            return L[mid][1]
+
+        if ( key < L[mid][0] ):
+            hi = mid
+        else:
+            lo = mid
+    return -1
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description = 'Inputs for plotting gene'
@@ -36,6 +59,8 @@ def parse_arguments():
     parser.add_argument('--gene', type = str, help = 'Name of gene to analyze', required = True)
     
     parser.add_argument('--sample_type', type = str, help = 'Group of samples to analyze gene expression for', required = True)
+    
+    parser.add_argument('--output_filename', type = str, help = 'Boxplot File Name', required = False)
     
     arguments = parser.parse_args()
     
@@ -109,6 +134,7 @@ def main():
     
     group_counts = [ [] for i in range(len(groups)) ]
     
+
     for l in gzip.open(data_file_name, 'rt'):
         if version == None:
             version = l
@@ -119,7 +145,13 @@ def main():
             continue
     
         if data_header == None:
-            data_header = l.rstrip().split('\t')
+            data_header = []
+            i = 0
+            for field in l.rstrip().split('\t'):
+                data_header.append([field, i])
+                i += 1
+            data_header.sort(key=lambda tup: tup[0])
+    
             continue
     
         A = l.rstrip().split('\t')
@@ -127,12 +159,12 @@ def main():
         if A[gene_name_col] == gene_name:
             for group_idx in range(len(groups)):
                 for member in members[group_idx]:
-                    member_idx = linear_search(member, data_header)
+                    member_idx = binary_search(member, data_header)
                     if member_idx != -1:
                         group_counts[group_idx].append(int(A[member_idx]))
-            break
+            break 
     
-    dv.boxplot(group_counts,groups,y_label = 'Gene Read Counts', xlabel = arguments.sample_type, title = arguments.gene)
+    dv.boxplot(group_counts,groups,ylabel = 'Gene Read Counts', xlabel = arguments.sample_type, title = arguments.gene, out_file_name = arguments.output_filename)
 
 if __name__ == '__main__':
     main()
--- test_plot_gtex.py
+++ test_plot_gtex.py
@@ -13,7 +13,21 @@ class TestLinearSearch(unittest.TestCase):
     def test_no_hit(self):
         self.assertEqual(pgtx.linear_search('aa',[1,2,3]),-1)
     def test_hit(self):
-        self.assertEqual(pgtx.linear_search(1,[1,2,3]),0)        
+        self.assertEqual(pgtx.linear_search(1,[1,2,3]),0)
+
+class TestBinarySearch(unittest.TestCase):
+    def test_input_type_key(self):
+        self.assertRaises(TypeError, pgtx.binary_search,None,[1,2,3])
+        self.assertRaises(TypeError,pgtx.binary_search,[1,2,3],[1,2,3])
+
+    def test_input_type_L(self):
+        self.assertRaises(TypeError, pgtx.binary_search,1,None)
+        self.assertRaises(TypeError,pgtx.binary_search,1,1)
+        self.assertRaises(TypeError,pgtx.binary_search,1,'test')
+    def test_no_hit(self):
+        self.assertEqual(pgtx.binary_search('aa',[1,2,3]),-1)
+    def test_hit(self):
+        self.assertEqual(pgtx.binary_search(1,[1,2,3]),0)               
 
 class TestSampleFile(unittest.TestCase):
     def test_file_type(self):
